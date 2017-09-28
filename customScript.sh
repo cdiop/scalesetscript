0d7347ff8a28f7758b3f8ab1ce43bdca86884329
Code <code.diop@activeeon.com>
2017-09-28 14:21:56 +0000

    wget added

--- customScript.sh
+++ customScript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo ooo > /tmp/op
-mkdir /tmp/test
+wget -nv http://maas-1.activeeon.com:8080/rest/node.jar
+
+java -jar node.jar -Dproactive.communication.protocol=pamr -Dproactive.pamr.router.address=maas-1.activeeon.com -Dproactive.pamr.router.port=33647 additionalProperties –r http://maas-1.activeeon.com:8080/rm –s scale-set-code –w 1
