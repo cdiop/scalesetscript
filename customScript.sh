e8952ff0c904e4780db209954293e51a05fcdc30
Code <code.diop@activeeon.com>
2017-10-02 17:00:22 +0000

    update curl command

--- customScript.sh
+++ customScript.sh
@@ -2,6 +2,5 @@
 
 wget -nv http://maas-1.activeeon.com:8080/rest/node.jar
 
-curl -k -X GET 'https://nsqueue.queue.core.windows.net/tata/messages?peekonly=false&numofmessages=1&sv=2017-04-17&ss=bfqt&srt=sco&sp=rwdlacup&se=2018-09-29T08:22:00Z&st=2017-09-29T00:22:00Z&spr=https&sig=SKhVeSqJL9SuZXqfUNnOstweGnRyxdNhDz1kThAUOKA%3D'
-
+curl -k -X GET ‘https://nsqueue.queue.core.windows.net/brian/messages?visibilitytimeout=120&numofmessages=1&sv=2017-04-17&ss=bfqt&srt=sco&sp=rwdlacup&se=2018-09-29T08:22:00Z&st=2017-09-29T00:22:00Z&spr=https&sig=SKhVeSqJL9SuZXqfUNnOstweGnRyxdNhDz1kThAUOKA%3D'
 
